f817b2766cac4946086d6d04f05527caa804393a
manthan <manthan.jsharma@gmail.com>
2025-05-03 15:25:11 +0530

    Update: SimulationSettings

--- app/dentalsimulator/page.tsx
+++ app/dentalsimulator/page.tsx
@@ -5,7 +5,6 @@ import DentalSimulation from "@/components/dental-simulator/dental-simulation";
 import Sidebar from "@/components/dental-simulator/sidebar";
 import Toolbar from "@/components/dental-simulator/toolbar";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { SimulationSettingsProvider } from "@/hooks/use-simulation-settings";
 
 export default function Home() {
   const [activeModel, setActiveModel] = useState("overbite");
@@ -13,29 +12,27 @@ export default function Home() {
   const [showSettings, setShowSettings] = useState(false);
 
   return (
-    <SimulationSettingsProvider>
-      <main className="flex h-screen flex-col bg-gray-100 dark:bg-gray-900">
-        <TooltipProvider>
-          <Toolbar
-            showSettings={showSettings}
-            setShowSettings={setShowSettings}
+    <main className="flex h-screen flex-col bg-gray-100 dark:bg-gray-900">
+      <TooltipProvider>
+        <Toolbar
+          showSettings={showSettings}
+          setShowSettings={setShowSettings}
+        />
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar
+            activeModel={activeModel}
+            setActiveModel={setActiveModel}
+            activeTreatment={activeTreatment}
+            setActiveTreatment={setActiveTreatment}
           />
-          <div className="flex flex-1 overflow-hidden">
-            <Sidebar
+          <div className="flex-1 relative">
+            <DentalSimulation
               activeModel={activeModel}
-              setActiveModel={setActiveModel}
               activeTreatment={activeTreatment}
-              setActiveTreatment={setActiveTreatment}
             />
-            <div className="flex-1 relative">
-              <DentalSimulation
-                activeModel={activeModel}
-                activeTreatment={activeTreatment}
-              />
-            </div>
           </div>
-        </TooltipProvider>
-      </main>
-    </SimulationSettingsProvider>
+        </div>
+      </TooltipProvider>
+    </main>
   );
 }
