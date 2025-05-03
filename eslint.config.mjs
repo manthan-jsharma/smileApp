908cd3d0533a8a86ca5b85f0db0592d0f4792260
manthan <manthan.jsharma@gmail.com>
2025-05-03 23:22:18 +0530

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off",
+      "react/jsx-no-comment-textnodes": "off",
     },
   },
 
